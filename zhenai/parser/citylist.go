3568ad2022ea3acd06cf947002a890fc922b04e2
flands <czx.flands@gmail.com>
2019-04-11 22:42:09 +0800

    1

---
+++ zhenai/parser/citylist.go
@@ -0,0 +1,28 @@
+package parser
+
+import (
+	"crawler/engine"
+	"regexp"
+)
+
+const cityListRe1 = `cityData.+footerData`
+const cityListRe2 = `(?:linkContent:")(.{0,8})(?:",linkURL:")(http://m.zhenai.com/zhenghun/[0-9a-z]+)"`
+
+func ParseCityList(contents []byte) engine.ParseResult {
+	re1 := regexp.MustCompile(cityListRe1)
+	re2 := regexp.MustCompile(cityListRe2)
+
+	matches1 := re1.FindAll(contents, -1)[0]
+	matches2 := re2.FindAllSubmatch(matches1, -1)
+
+	result := engine.ParseResult{}
+	for _, m := range matches2 {
+		result.Items = append(result.Items, string(m[1]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        string(m[2]),
+			ParserFunc: engine.NilParser,
+		})
+	}
+
+	return result
+}
